81a87c25dc00fa73c51aafd24b5423a50a05dda2
Romi <noya.cohen@post.runi.ac.il>
2025-10-31 20:19:59 +0200

    hw1

--- FVCalc.java
+++ FVCalc.java
@@ -5,6 +5,6 @@ public class FVCalc {
 	double Precent = Double.parseDouble(args[1]);
 	int Years = Integer.parseInt(args[2]);
 	double futurevalue = CurrentValue*Math.pow((1+Precent/100), Years);
-	System.out.println("After "+ Years +" years, "+ "$ " CurrentValue +"Saved at "+ Precent +"% "+ "will yiels "+ futurevalue +"$");
+	System.out.println("After "+ Years +" years, "+ "$ " + CurrentValue +" Saved at "+ Precent +"% "+ " will yiels "+ (int)futurevalue +"$");
 	}
 }
